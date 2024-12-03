ee0f51ca642ada8f5cacde2fbcbcfb0f38b0bdd1
Sergey <>
2024-12-03 15:40:25 +0300

    WIP iterators

--- 3. Code/StarFinder/Model/Model.h
+++ 3. Code/StarFinder/Model/Model.h
@@ -7,18 +7,45 @@ namespace Model {
         unsigned char green;
         unsigned char blue;
         unsigned char alpha;
-        virtual ~Pixel() = 0;
+        virtual ~Pixel() noexcept = 0;
     };
 
+    template <typename T>
     class ImageIterator {
-
+    public:
+        virtual T& operator*() = 0;
+        virtual ImageIterator& operator++() = 0;
+        virtual bool operator!=(const ImageIterator& rhs) const noexcept = 0;
+        virtual DataStructures::Coordinates get_coordinates() const = 0;
     };
+
+    // TODO
+    typedef ImageIterator<Pixel> image_iterator;
+    typedef ImageIterator<const Pixel> const_image_iterator;
 
     class Image {
     public:
         const size_t size;
 
-        virtual void outline_star(DataStructures::Coordinates center) = 0;
-        virtual Pixel& operator[](DataStructures::Coordinates) = 0;
+        virtual void outline_star(DataStructures::Coordinates center) noexcept = 0;
+        virtual Pixel& operator[](DataStructures::Coordinates) noexcept = 0;
+        virtual image_iterator begin() noexcept = 0;
+        virtual image_iterator end() noexcept = 0;
+        virtual const_image_iterator begin() const noexcept = 0;
+        virtual const_image_iterator end() const noexcept = 0;
+    };
+
+    class Star {
+    public:
+    };
+
+    class StarsIterator {
+    public:
+
+    };
+
+    class Stars {
+    public:
+        
     };
 }

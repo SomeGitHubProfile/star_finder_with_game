f54a76fc91e152a1e54cba544248ad8afd2a6f13
Sergey <fsrs39230020@gse.cs.msu.ru>
2024-12-28 01:15:49 +0300

    stable

--- 3. Code/StarFinder/main.cpp
+++ 3. Code/StarFinder/main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 
 #include "Errors/Errors.h"
+#include "Model/BaseModel/Params/SourceImage/BaseSourceImageParams.h"
+#include "Model/BaseModel/SourceImage/BaseSourceImage.h"
+#include "Model/BaseModel/StarFinder/BaseStarFinder.h"
 
 
 using namespace std;
@@ -12,7 +15,7 @@ int main()
 	cin >> s;
 	cout << "Hello " << s << '\n';
 	try {
-		/*model::base::params::BaseSourceImageParams image_params{s.c_str(), pixels::RGB_Pixel(), pixels::RGB_Pixel(), 0};
+		model::base::params::BaseSourceImageParams image_params{s.c_str(), pixels::RGB_Pixel(), pixels::RGB_Pixel(), 0};
 		image_params.init_source_image_dto();
 		model::SourceImage* image = new model::base::BaseSourceImage(image_params);
 		model::base::params::BaseStarFinderParams params{image, 5};
@@ -22,7 +25,7 @@ int main()
 		cout << "Number of stars: " << stars->get_size() << '\n';
 		delete stars;
 		delete image;
-		delete star_finder;*/
+		delete star_finder;
 		cout << "Without errors\n";
 	} catch (const errors::BaseError& e) {
 		cout << "Error catched " << e.text << '\n';
